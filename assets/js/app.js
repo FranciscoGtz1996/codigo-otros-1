0743aa634a80ef4cc3d2baf25d0b95da58a2678a
Francisco Gutierrez <franciscojavier.gutierrezrdgz@gmail.com>
2025-07-13 13:12:02 -0600

    added changes to index.html and app.js files

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,17 +2,18 @@
 
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const n = document.querySelector('.name'); /*Agregue . para seleccionar el elemento html y elimine el $ de la const n*/
+const b = document.querySelector('.blog'); /*Agregue . para seleccionar el elemento del html y elimine el $ de la const b*/
+const l = document.querySelector('.location'); /*Elimine el $ de la constante l */
 
 async function displayUser(username) { /* Agregue la palabra async function, para que fuera función promesa*/
-  $n.textContent = 'cargando...';
+  n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json(); /*Agregue data json() */
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  n.textContent = `${data.name}`; /*Agregue backticks */
+  b.textContent = `${data.blog}`; /**Agregue backticks */
+  l.textContent = `${data.location}`; /*Agregue backticks */
 }
 
 function handleError(err) {
